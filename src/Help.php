612d4e4c2d648eb1cdb22c9fda26f932b4b4b464
Gabriel Souza <gabriel@vistaonline.com.br>
2019-09-06 09:43:55 -0300

    first commit

---
+++ src/Help.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Api;
+
+class Help
+{
+
+    public function __construct(){
+
+        
+    }
+
+    // function de teste de modulo
+    public function hello(){
+        echo "Modulo OK";
+    }
+
+
+
+}
---
+++ teste.php
@@ -0,0 +1,11 @@
+<?php
+require_once './vendor/autoload.php'; // Autoload files using 
+
+
+use Api\Help;
+
+
+
+$a = new Help();
+
+$a->hello();
